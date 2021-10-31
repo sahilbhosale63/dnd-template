a957f0c800afc9067558c30371d329259bdb06ca
Sahil Bhosale <sahilbhosale63@live.com>
2021-10-31 11:35:08 +0530

    added mouse down code

--- dnd.js
+++ dnd.js
@@ -1,8 +1,23 @@
-function mousedown(){
+startAct();
+function startAct(){
 
+    let dragElements = document.getElementsByClassName("drag");
+    for(let i=0; i<dragElements.length; i++){
+        dragElements[i].addEventListener("mousedown", mousedown);
+    }
 }
 
-function mousemove(){
+function mousedown(event){
+
+    event.target.addEventListener("mousemove", mousemove)
+    
+}
+
+function mousemove(event){
+
+    console.log(event.clientX, event.clientY)
+    event.target.style.top = event.clientX;
+    event.target.style.left = event.clientY;
     
 }
 
